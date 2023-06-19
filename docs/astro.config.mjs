c25deb055b2a1235e9fee27410967e5fb05119cb
Matt Snelgrove <matt.snelgrove@gmail.com>
2023-06-19 12:22:37 +1000

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig( {
+  site: "https://snelgrove.github.io",
+  base: "starlight-test",
   integrations: [
     starlight({
       title: 'My Docs',
